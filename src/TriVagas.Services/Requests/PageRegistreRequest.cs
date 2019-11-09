f1812cdf177798afa8b5cffbaeebb295a12c6afa
dwenndson <diego.wenndson@gmail.com>
2019-11-09 12:18:24 -0300

    adicionando a pageController

---
+++ src/TriVagas.Services/Requests/PageRegistreRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TriVagas.Domain.Models;
+
+namespace TriVagas.Services.Requests
+{
+    public class PageRegistreRequest
+    {
+        public Company Company { get; set; }
+        public Page Page { get; set; }
+        public PageUser PageUser { get; private set; }
+       
+    }
+}
---
+++ src/TriVagas.WebApi/Controllers/PageController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TriVagas.Services.Requests;
+
+namespace TriVagas.Application.Controllers
+{
+    [Produces("application/json")]
+    [Route("page/")]
+    [ApiController]
+    public class PageController : ControllerBase
+    {
+        [HttpPost]
+        [Route("createpage")]
+        public IActionResult RegistrePage([FromBody] PageRegistreRequest page )
+        {
+            return Ok(page); 
+        }
+    }
+}
+
