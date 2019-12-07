cb1c4bca272e442b923369a486aee39d3838ed84
Alan Soder <alanbr4@gmail.com>
2019-12-07 14:09:14 -0300

    Remoção pasta .vscode e comentarios

--- src/TriVagas.Infra.Data/Repository/UserRepository.cs
+++ src/TriVagas.Infra.Data/Repository/UserRepository.cs
@@ -1,25 +1,24 @@
 using TriVagas.Domain.Interfaces;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
-using TriVagas.Domain.Interfaces;
 using TriVagas.Domain.Models;
 using TriVagas.Infra.Data.Context;
 
 namespace TriVagas.Infra.Data.Repository
 {
-    public class UserRepository : BaseRepository<User>, IUserRepository
+  public class UserRepository : BaseRepository<User>, IUserRepository
+  {
+    public UserRepository(DataContext context) : base(context)
     {
-        public UserRepository(DataContext context) : base(context)
-        {
-        }
-        public async Task<User> Login(string email, string password)
-        {
-            return await DbSet.AsNoTracking().FirstOrDefaultAsync(u => u.Email == email && u.Password == password);
-        }
-
-        public async Task<User> GetByEmail(string email)
-        {
-            return await DbSet.FirstOrDefaultAsync(u => u.Email == email);
-        }
     }
+    public async Task<User> Login(string email, string password)
+    {
+      return await DbSet.AsNoTracking().FirstOrDefaultAsync(u => u.Email == email && u.Password == password);
+    }
+
+    public async Task<User> GetByEmail(string email)
+    {
+      return await DbSet.FirstOrDefaultAsync(u => u.Email == email);
+    }
+  }
 }
--- src/TriVagas.WebApi/Filters/JWTokenAuthFilter.cs
+++ src/TriVagas.WebApi/Filters/JWTokenAuthFilter.cs
@@ -6,43 +6,33 @@ using TriVagas.WebApi.HttpResults;
 
 namespace TriVagas.WebApi.Filters
 {
-    public class JWTokenAuthFilter : IAuthorizationFilter
+  public class JWTokenAuthFilter : IAuthorizationFilter
+  {
+    private readonly IJWTService _jwtService;
+    private readonly IUserService _userService;
+
+    public JWTokenAuthFilter(IJWTService jwtService, IUserService userService)
     {
-        private readonly IJWTService _jwtService;
-        private readonly IUserService _userService;
-
-        public JWTokenAuthFilter(IJWTService jwtService, IUserService userService)
-        {
-            _jwtService = jwtService;
-            _userService = userService;
-        }
-
-        public async void OnAuthorization(AuthorizationFilterContext context)
-        {
-            var token = context.HttpContext.Request.Headers["JWToken"];
-            try
-            {
-                if (!_jwtService.IsTokenValid(token))
-                {
-                    context.Result = new HttpForbiddenResult();
-                    return;
-                }
-
-                //var claims = _jwtService.GetTokenClaims(token);
-                //var email = claims.First().Value;
-                //var user = await _userService.GetByEmail(email);
-
-                //if (user == null)
-                //{
-                //    context.Result = new HttpForbiddenResult();
-                //    return;
-                //}
-            }
-            catch (Exception ex)
-            {
-                context.Result = new HttpForbiddenResult();
-                return;
-            }
-        }
+      _jwtService = jwtService;
+      _userService = userService;
     }
+
+    public void OnAuthorization(AuthorizationFilterContext context)
+    {
+      var token = context.HttpContext.Request.Headers["JWToken"];
+      try
+      {
+        if (!_jwtService.IsTokenValid(token))
+        {
+          context.Result = new HttpForbiddenResult();
+          return;
+        }
+      }
+      catch
+      {
+        context.Result = new HttpForbiddenResult();
+        return;
+      }
+    }
+  }
 }
